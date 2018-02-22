5af570c1f6353dacfed0c52dcf76fd36b2a7247c
dorisobor <troyamber@live.com>
2018-02-22 14:01:42 +0100

    added delete and check image

--- main.js
+++ main.js
@@ -1,35 +1,49 @@
-// get the id from the button 
-const button = document
-  .getElementById("button");
+// gets the id from the add button 
+const buttonAdd = document
+  .getElementById("buttonAdd");
 
-// get the id from the refresh button 
+// gets the id from the refresh button 
 const refresh = document
   .getElementById("refresh");
 
-
 //creates a function to the button so that you can submit the task
-button.addEventListener("click", function () {
+buttonAdd.addEventListener("click", function () {
 
   /*  gets the variable x that is the
    id of text input. The value will be the 
    text we put in the field */
   var x = document.getElementById("todoText").value;
 
-  /* creates a paragraph tag so
+  /* creates a li tag so
       you dont have to do it in html  */
-  const paragraph = document
-    .createElement("p");
+  const todoTask = document
+    .createElement("li");
+  // creates a class for li so it can be styled to css
+  todoTask.classList.add('todoTask');
 
-  // creates a class for paragraph so it can be styled to css
-  paragraph.classList.add('todo-item');
-  paragraph.innerHTML = x;
-  document.getElementById('todos').appendChild(paragraph);
+  // delete button
+  const deleteImage = document
+    .createElement("img");
+  // sets css attributes to the img
+  deleteImage.setAttribute("src", "images/delete.png");
+  deleteImage.setAttribute("height", "25");
+  deleteImage.setAttribute("width", "25");
+  deleteImage.classList.add("deleteImage");
+
+
+
+
+  //gets the value from the field and adds it to the list
+  todoTask.innerHTML = x;
+  document.getElementById('todoList').appendChild(todoTask);
+  todoTask.appendChild(deleteImage);
+
 });
 
 
 // refresh function for the list
 refresh.addEventListener('click', function () {
-  var list = document.getElementsByTagName("p");
+  var list = document.getElementsByTagName("li");
 
   for (var i = 0; i < list.length; i += 0) {
     list[i].remove();
@@ -37,3 +51,8 @@ refresh.addEventListener('click', function () {
 
 
 });
+
+
+
+
+
